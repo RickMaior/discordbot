1422b42c62f43531baa667e5ce0818ed1a72c30a
Rick <ricardoxmatox@hotmail.com>
2019-07-30 00:33:34 +0100

    nao sei

--- commands/youtubeLink.js
+++ commands/youtubeLink.js
@@ -23,65 +23,74 @@ module.exports.run = async (bot, message, args) => {
   //     console.log(`The video's url is: ${video[0].url}`);
 
   // // ^ procura e em baixo comando anterior
-  
-  // const { voiceChannel } = message.member;
-  // if (!args[0]) {
-  //   if (bot.queue.length !== 0) {
-  //     message.channel.send("There is queue");
-  //   } else {
-  //     message.channel.send("There is no queue");
-  //   }
-  //   console.log("pls give me something");
-  // } else {
-  //   switch (args[0]) {
-  //     case "next":
-  //       let value = bot.queue.shift();
-  //       if (value) {
-  //         message.channel.send(
-  //           `Playing now: **${value.title}** submited by <@${value.userID}>`
-  //         );
-  //       } else {
-  //         message.channel.send("No musics on queue");
-  //         if(bot.queue.length === 0){voiceChannel.leave()}
-  //       }
-  //       break;
-  //     case "delete":
-  //       bot.queue = [];
-  //       message.reply("The queue was deleted");
-  //       break;
-  //     case "add":
-  //       //  if(bot.queue.length === 0){
-  //       //   if (!voiceChannel) {
-  //       //     return message.reply("please join a voice channel first!");
-  //       //   }else{voiceChannel.join()}
-  //       // }
-  //       let info = await getInfo(args.slice(1).join(" "));
-  //       console.log("info.items= " + info.items);
-  //       console.log("O 2  titulo é: " + info.items[1].title);
-  //       //for( let i=0 ;i < 5 ;i++ ){
-  //        //await  console.log("Os 5 primeiros titulos sao: " + info.items[i].title )
-  //       //}
-  //       await bot.queue.push({
-  //         userID: message.author.id,
-  //         title: info.items[0].title,
-  //         yturl: info.items[0].webpage_url,
-  //         url: info.items[0].url
-  //         //discordMusic: something
-  //       });
-  //       await message.channel.send(
-  //         `The music **${info.items[0].title}** was added by __<@${
-  //           message.author.id
-  //         }>__ and have the link ${info.items[0].webpage_url} with url ${
-  //           info.items[0].url
-  //         }`
-  //       );
-  //       break;
-  //     case "play":
-  //       break;
-  //     default:
-  //       message.channel.send("Please use a valid command");
-  //   }
-  // }
+
+  let queue = bot.queue
+  const { voiceChannel } = message.member;
+  if (!args[0]) {
+    if (queue.length !== 0) {
+      message.channel.send("There is queue");
+    } else {
+      message.channel.send("There is no queue");
+    }
+    console.log("pls give me something");
+  } else {
+    switch (args[0]) {
+      case "next":
+        let value = queue.shift();
+        if (value) {
+          message.channel.send(
+            `Playing now: **${value.title}** submited by <@${value.userID}>`
+          );
+        } else {
+          message.channel.send("No musics on queue");
+          if (queue.length === 0) {
+            voiceChannel.leave();
+          }
+        }
+        break;
+      case "delete":
+        queue = [];
+        message.reply("The queue was deleted");
+        break;
+      case "add":
+        //  if(queue.length === 0){
+        //   if (!voiceChannel) {
+        //     return message.reply("please join a voice channel first!");
+        //   }else{voiceChannel.join()}
+        // }
+        let info = await getInfo(args.slice(1).join(" "));
+
+
+        //console.log("O 2  titulo é: " + info.items[0].title);
+        await queue.push({
+          userID: message.author.id,
+          title: info.items[0].title,
+          yturl: info.items[0].webpage_url,
+          url: info.items[0].url
+          //discordMusic: something
+        });
+
+        const embed = new Discord.RichEmbed()
+        .setTitle("Youtube")
+        .setImage(info.items[0].thumbnail)
+        .addField("The music",`[info.items[0].title](https://google.com)`)
+        .addField( " has been added by ", "<@" + message.author.id + ">")
+
+        await message.channel.send(embed)
+
+        // await message.channel.send(
+        //   `The music **${info.items[0].title}** was added by __<@${
+        //     message.author.id
+        //   }>__ and have the link ${info.items[0].webpage_url}`
+        // );
+
+        break;
+      case "play":
+        break;
+      default:
+        message.channel.send("Please use a valid command");
+    }
+  }
 };
 
 module.exports.help = {
