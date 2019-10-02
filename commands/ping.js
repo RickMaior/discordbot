97179c5b8143a0cb28cadabc106d75b8ac1dbcbc
Rick <ricardoxmatox@hotmail.com>
2019-10-02 21:07:46 +0100

    trying to fix this

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-const settings = require("../bot/settings.json");
-const Discord = require("discord.js");
 
-
-
-=======
->>>>>>> ffa36aecbd890c7ac56ae5da3448d65291487060
 
 module.exports.run = async (bot, message) => {
     message.channel.send("My ping is: "+ Math.round(bot.ping))
