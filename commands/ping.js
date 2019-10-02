4f66fe4337843075c9f5a1a022fcb630836b6ce6
Rick <ricardoxmatox@hotmail.com>
2019-10-02 20:53:01 +0100

    test to update local code

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
 const settings = require("../bot/settings.json");
 const Discord = require("discord.js");
-const fs = require("fs");
-const ms = require("ms");
-const { getInfo } = require('ytdl-getinfo')
+
+
+
 
 module.exports.run = async (bot, message, args) => {
     message.channel.send("My ping is: "+ Math.round(bot.ping))
