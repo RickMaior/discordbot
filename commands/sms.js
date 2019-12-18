32801f7885dde44aebb4e8f87515685ea2871b21
Ricardo Matos <ricardoxmatox@hotmail.com>
2019-12-18 01:59:58 +0000

    adicçao da versao do node

---
+++ commands/sms.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	
+
+};
+
+module.exports.help = {
+	name: "QR Code",
+	command: "qr",
+	aliases: ["code", "qrcode"],
+	helpInfo: ["give me a text, i give u a qr code"],
+	usage: "[text to put in qr]"
+};
