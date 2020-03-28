6e809ce62c4f06d91fb317050d77c605df0a488e
rick <ricardoxmarox@hotmail.com>
2020-03-28 02:12:31 +0000

    webhook with choise of link vs id/token

--- commands/webhook.js
+++ commands/webhook.js
@@ -1,9 +1,11 @@
 const { messageAuthorCollect } = require("../utils/messageFunctions.js");
 const webHookFn = require("../utils/webhookFunctions.js");
+const emojiCharacters = require("../emojiCharacters");
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    
+
 
 
 
@@ -13,26 +15,63 @@ module.exports.run = async (bot, message, args) => {
     if (!messageSend) return message.reply("Error getting the message")
     console.log(messageSend)
 
-
-    // const webHookData = await webHookFn.webHookByURL(message)
-    // console.log(webHookData)
+    // // // start
 
 
-    const webHookData = await webHookFn.webHookByIdToken(message)
-    console.log(webHookData)
+
+    const embed = new Discord.MessageEmbed()
+        .setColor('DEFAULT')
+        .setTitle('Sending webhook')
+        .setDescription(`react with ${emojiCharacters[1]} if you want to just give the link, or with  ${emojiCharacters[2]} if you want to give the Id and the Token`)
 
 
-    // test webhook id:     674439506623070238
-    //              token:  OJisYdcfA65s_P-z1eNfQCUUs5BayJeSRxioef4keozULqMzLY4A13c_S-jHiO5uFJ-V
+    let msg = await message.author.send(embed)
 
-    //can share  https://discordapp.com/api/webhooks/675509522193711104/EULdnScdEzzew5R0ybFjpKEemsCaIrO-A3_wsNG4E6RrSzRGYAIX3ecZ6a-i2eTNGPnK
 
-    // use an webhook by id and token
-    let webhook = await bot.fetchWebhook(webHookData.id, webHookData.token)
-        .then((webhook) => { 
+
+    //confirmçao se envia menssagem ou nao
+
+
+    msg.react(emojiCharacters[1]).then(() => msg.react(emojiCharacters[2]));
+
+    const filter = (reaction, user) => {
+        return [emojiCharacters[1], emojiCharacters[2]].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+
+    let webHookData
+    collected = await msg.awaitReactions(filter, { max: 1 })
+
+    const reaction = collected.first();
+
+    if (reaction.emoji.name === emojiCharacters[1]) {
+        //message.author.send('you picked by link');
+        webHookData = await webHookFn.webHookByURL(message)
+        console.log(webHookData)
+
+    } else {
+        //message.author.send('You picked by token/id');
+        webHookData = await webHookFn.webHookByIdToken(message)
+        console.log(webHookData)
+    }
+
+
+
+
+
+
+
+
+    // // test webhook id:     674439506623070238
+    // //              token:  OJisYdcfA65s_P-z1eNfQCUUs5BayJeSRxioef4keozULqMzLY4A13c_S-jHiO5uFJ-V
+
+    // //can share  https://discordapp.com/api/webhooks/675509522193711104/EULdnScdEzzew5R0ybFjpKEemsCaIrO-A3_wsNG4E6RrSzRGYAIX3ecZ6a-i2eTNGPnK
+
+    // // use an webhook by id and token
+    bot.fetchWebhook(webHookData.id, webHookData.token)
+        .then((webhook) => {
             webhook.send(messageSend);
             if (message.channel.type === "dm") { message.reply("WebHook sended") }
-         })
+        })
         .catch((err) => {
             message.reply("there was an error getting your webhook, verify you got the right values.")
             console.log(err)
@@ -40,17 +79,17 @@ module.exports.run = async (bot, message, args) => {
 
 
 
-    // // get true and falase for every webhook on a room
-    // let fetchWebhooks = await message.channel.fetchWebhooks();
-    // console.log("fetched:")
-    // console.log(fetchWebhooks)
-    // console.log("fetchWebhooks.owner:")
-    // fetchWebhooks.map((singleHook)=>console.log(singleHook.owner.id ===  bot.user.id))
+    // // // get true and falase for every webhook on a room
+    // // let fetchWebhooks = await message.channel.fetchWebhooks();
+    // // console.log("fetched:")
+    // // console.log(fetchWebhooks)
+    // // console.log("fetchWebhooks.owner:")
+    // // fetchWebhooks.map((singleHook)=>console.log(singleHook.owner.id ===  bot.user.id))
 
 
-    // // create a new webhook for the channel
-    //   let webhook = await  message.channel.createWebhook("test")
-    //   webhook.send(messageSend)
+    // // // create a new webhook for the channel
+    // //   let webhook = await  message.channel.createWebhook("test")
+    // //   webhook.send(messageSend)
 
 };
 
